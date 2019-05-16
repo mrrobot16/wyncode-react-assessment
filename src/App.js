14128f364aff4d95785f0130917e308c0fcde77e
Hector Garcia <hmgaar@gmail.com>
2019-05-16 17:17:55 -0400

    Finished creating all components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import './App.css';
+
 class Notification extends React.Component {
+  render(){
+    if(this.props.notification) {
+      const notification = this.props.notification
+      const message = notification ? notification.message : ''
+      return (
+        <React.Fragment>
+          <p>{message}</p>
+        </React.Fragment>
+      )
+    }
+    else {
+      return(null)
+    }
+  }
+}
+
+class Confirmation extends React.Component {
+  constructor() {
+    super()
+    this.state = { showConfirmation: true }
+  }
+
   alertStylings(alertType) {
     switch (alertType) {
       case 'success':
@@ -15,31 +38,38 @@ class Notification extends React.Component {
         return 'alert alert-info'
     }
   }
+
+  hideNotification(){
+    this.setState( { showConfirmation: false } )
+  }
+
   render(){
-    if(this.props.notification) {
-      const notification = this.props.notification
-      const message = notification ? notification.message : ''
-      const messageStyle = this.alertStylings(notification.type)
-      return (
+    const notification = this.props.notification
+    const messageStyle = notification ? this.alertStylings(notification.type) : 'alert alert-info'
+    if(this.props.notification && this.state.showConfirmation) {
+      return(
         <div className={messageStyle}>
-          { message }
+          <Notification notification={this.props.notification}/>
+            <div className="btn btn-primary" onClick={()=>this.hideNotification()}>Sure</div>
+            <div className="btn btn-danger" onClick={()=>this.hideNotification()}>No Thanks</div>
         </div>
       )
     }
     else {
       return(null)
     }
+
   }
 }
 class App extends React.Component {
   render(){
     return (
       <div className="App">
-        <Notification notification={ { message: 'Hello World - Success', type: 'success' } }/>
-        <Notification notification={ { message: 'Hello World - Message', type: 'message' } }/>
-        <Notification notification={ { message: 'Hello World - Cation', type: 'cation' } }/>
-        <Notification notification={ { message: 'Hello World - Error', type: 'error' } }/>
-        <Notification/>
+        <Confirmation notification={ { message: 'Hello World - Success', type: 'success' } }/>
+        <Confirmation notification={ { message: 'Hello World - Message', type: 'message' } }/>
+        <Confirmation notification={ { message: 'Hello World - Cation', type: 'cation' } }/>
+        <Confirmation notification={ { message: 'Hello World - Error', type: 'error' } }/>
+        <Confirmation/>
       </div>
     );
   }
