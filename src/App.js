72490c7430bf1f559ed31f18227ad7600048f665
Hector Garcia <hmgaar@gmail.com>
2019-05-16 16:55:37 -0400

    Wyncode React Assessment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Notification extends React.Component {
+  alertStylings(alertType) {
+    switch (alertType) {
+      case 'success':
+        return 'alert alert-success'
+      case 'message':
+        return 'alert alert-info'
+      case 'cation':
+        return 'alert alert-warning'
+      case 'error':
+        return 'alert alert-danger'
+      default:
+        return 'alert alert-info'
+    }
+  }
+  render(){
+    if(this.props.notification) {
+      const notification = this.props.notification
+      const message = notification ? notification.message : ''
+      const messageStyle = this.alertStylings(notification.type)
+      return (
+        <div className={messageStyle}>
+          { message }
+        </div>
+      )
+    }
+    else {
+      return(null)
+    }
+  }
+}
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <Notification notification={ { message: 'Hello World - Success', type: 'success' } }/>
+        <Notification notification={ { message: 'Hello World - Message', type: 'message' } }/>
+        <Notification notification={ { message: 'Hello World - Cation', type: 'cation' } }/>
+        <Notification notification={ { message: 'Hello World - Error', type: 'error' } }/>
+        <Notification/>
+      </div>
+    );
+  }
 }
 
 export default App;
